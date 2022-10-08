3b187217442ecd43ec9242688c2ed9fb1a714efd
Oleksandr <voronko.oleksandr@gmail.com>
2022-10-08 19:50:53 +0300

    create sign up form and set its style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
